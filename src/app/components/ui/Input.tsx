867f4c3c344984d3e8b4c8ceaf6fe3a06efdc60e
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-04 23:52:51 -0300

    add components shadcn

--- src/app/components/ui/Input.tsx
+++ src/app/components/ui/Input.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
-import { InputHTMLAttributes } from "react";
+import * as React from "react"
+
+import { cn } from "~/lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -8,12 +9,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
-        className="py-2 px-3 rounded-md text-black bg-white border border-spacing-[1px] border-zinc-700"
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
         ref={ref}
         {...props}
       />
-    );
+    )
   }
-);
-Input.displayName = "Input";
-export { Input };
+)
+Input.displayName = "Input"
+
+export { Input }
