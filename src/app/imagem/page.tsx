ea53e24a964e33baba4cbb743c96d3b00fcc4177
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-06 22:13:04 -0300

    add pages image and perfil

---
+++ src/app/imagem/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "~/firebase/firebaseConnec";
+import { doc, setDoc } from "firebase/firestore";
+
+import { AuthContext } from "~/context/context";
+import { z } from "zod";
+
+import { useContext, useState } from "react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Progress } from "~/components/ui/progress";
+import { Textarea } from "~/components/ui/textarea";
+import { useAddImageSchema } from "~/components/schema/schemaAddImage";
+
+export default function Imagem() {
+  const { userAuth } = useContext(AuthContext);
+  const [progress, setProgress] = useState(0);
+
+  const { handleSubmit, schema, errors, register } = useAddImageSchema();
+  type formDataProps = z.infer<typeof schema>;
+
+  function extrairToken(url: string) {
+    const regex = /token=([^&]+)/;
+    const match = url.match(regex);
+
+    if (match) {
+      return match[1];
+    }
+    return "";
+  }
+
+  const uploadImage = (data: formDataProps) => {
+    const { description, image, title } = data;
+    const file = image[0];
+
+    if (!file) return;
+    const storageRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (err) => {
+        console.log(err);
+      },
+      async () => {
+        var urlImage = "";
+        await getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          urlImage = url;
+        });
+
+        const imgID = extrairToken(urlImage);
+
+        await setDoc(doc(db, "images", imgID), {
+          urlImage,
+          imgID,
+          userID: userAuth?.UserID,
+          description,
+          title,
+        });
+      }
+    );
+  };
+  return (
+    <main className="flex flex-col items-center h-screen gap-7">
+      <div className="flex items-start">
+        <span className="text-4xl font-medium">Criar uma nova imagem</span>
+      </div>
+      <form
+        className="grid gap-4 py-4 justify-center"
+        onSubmit={handleSubmit(uploadImage)}
+      >
+        <label className="flex flex-col gap-2" htmlFor="title">
+          <span>Titúlo</span>
+          <Input id="title" type="text" {...register("title")} />
+          {errors.title && (
+            <span className="text-red-700 text-xs">{errors.title.message}</span>
+          )}
+        </label>
+
+        <label className="flex flex-col gap-2" htmlFor="description">
+          <span>Descrição da imagem</span>
+          <Textarea id="description" {...register("description")}></Textarea>
+          {errors.description && (
+            <span className="text-red-700 text-xs">
+              {errors.description.message}
+            </span>
+          )}
+        </label>
+
+        <label className="flex flex-col gap-2" htmlFor="file">
+          <span>Escolha uma imagem</span>
+          <Input
+            id="file"
+            type="file"
+            {...register("image")}
+            accept="image/png, image/jpeg, image/jpg"
+          />
+        </label>
+
+        <div className="flex items-center gap-2">
+          <Progress value={progress} max={100} /> <span>{progress}%</span>
+        </div>
+
+        <div className="flex justify-center items-center flex-row gap-3 w-full">
+          <Button type="submit" className="bg-green-900 w-full">
+            Salvar
+          </Button>
+        </div>
+      </form>
+    </main>
+  );
+}
---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "~/context/context";
+
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { MdArrowBack } from "react-icons/md";
+import { useRouter } from "next/navigation";
+import { imagesProps } from "../@types/imagetype";
+import { getImagesUser } from "~/helpers/getImagesUser";
+
+export default function Perfil() {
+  const { userAuth } = useContext(AuthContext);
+  const { back, push } = useRouter();
+
+  const [SavedAndCreated, setSavedAndCreated] = useState(true);
+  const [images, setImages] = useState<imagesProps[]>([]);
+
+  function toAlterSessionSavedAndCreated() {
+    setSavedAndCreated(!SavedAndCreated);
+  }
+
+  useEffect(() => {
+    if (userAuth === undefined) return;
+
+    getImagesUser(setImages, userAuth.UserID);
+  }, [userAuth]);
+
+  return (
+    <main className="flex flex-col items-center h-screen gap-7">
+      <section className="flex items-center justify-center flex-col gap-10 w-[1200px]">
+        <div className="flex items-center flex-col gap-4">
+          <span className="w-28 h-28 rounded-full bg-zinc-200 flex items-center justify-center text-6xl font-semibold">
+            {userAuth?.name.substring(0, 1).toLocaleUpperCase()}
+          </span>
+
+          <span className="text-4xl">{userAuth?.name}</span>
+
+          <div className="flex items-center justify-center gap-1">
+            <span className="flex items-center justify-center font-medium text-xs w-[20px] h-[20px] bg-gray-400 text-white rounded-full">
+              IS
+            </span>
+            <span>{userAuth?.user}</span>
+          </div>
+        </div>
+
+        <div>
+          <Button variant={"default"}>Editar perfil</Button>
+        </div>
+
+        <div>
+          <Button
+            onClick={toAlterSessionSavedAndCreated}
+            variant={`${SavedAndCreated ? "link" : "ghost"}`}
+            className={`${SavedAndCreated ? "underline" : ""}`}
+          >
+            Criados
+          </Button>
+          <Button
+            onClick={toAlterSessionSavedAndCreated}
+            variant={`${!SavedAndCreated ? "link" : "ghost"}`}
+            className={`${!SavedAndCreated ? "underline" : ""}`}
+          >
+            Salvos
+          </Button>
+        </div>
+
+        <div className="max-w-[1200px] w-[70%]">
+          <div className="w-full flex justify-center">
+            {SavedAndCreated === true ? (
+              <div className="flex flex-col items-center gap-4">
+                <div>
+                  <Button
+                    variant={"destructive"}
+                    onClick={() => push("/imagem")}
+                  >
+                    Criar imagem
+                  </Button>
+                </div>
+                <div className="max-w-[1200px] flex flex-wrap">
+                  {images.length > 0 ? (
+                    images.map((image) => (
+                      <div className="relative group" key={image.imgID}>
+                        <Image
+                          src={image.urlImage}
+                          width={100}
+                          height={150}
+                          alt=""
+                          className="rounded-lg mb-3 w-full bg-no-repeat"
+                        />
+                      </div>
+                    ))
+                  ) : (
+                    <p>
+                      Ainda não há nada para mostrar. As imagens que você criar
+                      ficarão aqui.
+                    </p>
+                  )}
+                </div>
+              </div>
+            ) : (
+              <p>Em construção</p>
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
