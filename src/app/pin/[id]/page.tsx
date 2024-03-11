b628aeac026dc0ad3d53376a5b9d212e9e360dd9
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-03-11 11:16:53 -0300

    improvement in image detail screen

--- src/app/pin/[id]/page.tsx
+++ src/app/pin/[id]/page.tsx
@@ -1,13 +1,103 @@
-export default function Pin({
+"use server";
+
+import { doc, getDoc } from "firebase/firestore";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  IoArrowBackOutline,
+  IoEllipsisHorizontalSharp,
+  IoShareSocialOutline,
+} from "react-icons/io5";
+import { ImageProps } from "~/app/@types/imagetype";
+import { Avatar } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
+import { Card } from "~/components/ui/card";
+import { Label } from "~/components/ui/label";
+import { db } from "~/firebase/firebaseConnec";
+
+export default async function Pin({
   params,
 }: {
   params: {
     id: string;
   };
 }) {
+  const { id } = params;
+
+  let image: ImageProps = {
+    imageName: "",
+    author: "",
+    title: "",
+    userID: "",
+    imgID: "",
+    description: "",
+    urlImage: "",
+  };
+
+  async function getImage() {
+    const img = await getDoc(doc(db, "images", id));
+
+    if (img.data() !== undefined) {
+      image = img.data() as ImageProps;
+    }
+  }
+
+  await getImage();
   return (
-    <section>
-      <span>{params.id}</span>
+    <section className="flex justify-around w-full max-w-[1300px]  mt-24">
+      <Button
+        variant={"outline"}
+        className="gap-2 absolute top-28 left-5"
+        asChild
+      >
+        <Link href={"/"}>
+          <IoArrowBackOutline size={20} />
+          Voltar
+        </Link>
+      </Button>
+      <Card className="w-3/4 flex flex-row rounded-3xl">
+        <div className="w-1/2 flex">
+          <Image
+            alt={image.imageName}
+            src={image.urlImage}
+            width={400}
+            height={500}
+            className="rounded-3xl rounded-e-none"
+          />
+        </div>
+
+        <div className="flex flex-col gap-10 items-start w-1/2 p-4">
+          <div className="w-full flex ">
+            <div className="w-1/2 flex justify-start">
+              <Button variant={"ghost"}>
+                <IoShareSocialOutline size={30} />
+              </Button>
+              <Button variant={"ghost"}>
+                <IoEllipsisHorizontalSharp size={30} />
+              </Button>
+            </div>
+
+            <div className="w-1/2 flex justify-end">
+              <Button variant={"destructive"}>Salvar</Button>
+            </div>
+          </div>
+
+          <p className="font-semibold text-xl">{image.title}</p>
+          <div className="flex flex-col gap-10">
+            <Label className="flex items-center gap-2">
+              <Avatar className="w-14 h-14 rounded-full bg-gray-800 text-white flex items-center justify-center text-4xl">
+                {image.author.substring(0, 1).toLocaleUpperCase()}
+              </Avatar>
+              <h1>{image.author}</h1>
+            </Label>
+
+            <Label>
+              <span className="text-lg">Descrição</span>
+              <p>{image.description}</p>
+            </Label>
+          </div>
+        </div>
+      </Card>
     </section>
   );
 }
